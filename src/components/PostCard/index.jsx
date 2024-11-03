d9933644630d221d7b5b7b5eac59a3a8717995a8
Rafael Cunha <rafael.bispocunha@outlook.com>
2024-11-03 11:02:27 -0300

    Initial

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+
+import "./styles.css";
+
+export const PostCard = ({ id, title, body, cover }) => (
+  <div className="post" key={id}>
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h2>{title}</h2>
+      <p>{body}</p>
+    </div>
+  </div>
+);
